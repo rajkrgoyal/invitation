5d608c9f54e6a54ccff85fa995c5e8e9d26d11b8
Raj Kumar Goyal <goyalkrraj@gmail.com>
2023-05-11 22:47:16 +0530

    fixed rubocop voilations for all the controllers

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,20 +1,12 @@
-class RegistrationsController < Devise::RegistrationsController
-  def new
-    super
-  end
+# frozen_string_literal: true
 
+class RegistrationsController < Devise::RegistrationsController
   def create
     super do |resource|
       email = Base64.decode64(params['user']['referred_by'].to_s)
-      user = User.find_by(email: email) rescue nil
-      referral = user && user.referrals.find_by(email: resource.email)
-      if referral
-        referral.update(status: :accepted)
-      end
+      user = User.find_by(email: email)
+      referral = user&.referrals&.find_by(email: resource.email)
+      referral&.update(status: :accepted)
     end
   end
-
-  def update
-    super
-  end
-end 
+end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 class UserController < ApplicationController
-  def index
-  end
+  def index; end
 
   def send_referral
     referral = Referral.new(email: params[:email], user_id: current_user.id, status: :pending)
 
     if referral.save
       send_email(referral.email)
-      flash[:notice] = "Referral sent successfully."
+      flash[:notice] = 'Referral sent successfully.'
     else
-      flash[:error] = "Error sending referral."
+      flash[:error] = 'Error sending referral.'
     end
+
     redirect_to root_path
   end
 
@@ -20,15 +22,16 @@ class UserController < ApplicationController
       send_email(referral.email)
       flash[:notice] = "Invitation has been resent to #{referral.email}."
     else
-      flash[:error] = "Error resending referral."
+      flash[:error] = 'Error resending referral.'
     end
+
     redirect_to root_path
   end
 
   private
 
   def send_email(email)
-    url = request.base_url + '/users/sign_up?referred_by=' + Base64.encode64(current_user.email)
-    SendReferralMailer.send_email(email, {url: url}).deliver_now
+    url = "#{request.base_url}/users/sign_up?referred_by=#{Base64.encode64(current_user.email)}"
+    SendReferralMailer.send_email(email, { url: url }).deliver_now
   end
 end
