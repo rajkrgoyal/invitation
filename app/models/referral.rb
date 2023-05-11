3e08136ab658789318d89bf475f78d577fcf8c12
Raj Kumar Goyal <goyalkrraj@gmail.com>
2023-05-11 22:49:15 +0530

    fixed rubocop voilations for all the models

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 class Referral < ApplicationRecord
   belongs_to :user
 
-  enum status: [:pending, :accepted]
+  enum status: %i[pending accepted]
   validates :email, uniqueness: { scope: :user_id }
   validates :resend_count, numericality: { less_than_or_equal_to: 5 }
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum role: [:admin, :referral]
+  enum role: %i[admin referral]
 
   has_many :referrals
 
